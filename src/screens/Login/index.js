8f34b466e7fff1a6a8196efcf4500fee23256fd5
Jean Silva <jeansilva@MacBook-Pro-de-Jean.local>
2020-07-27 22:47:14 -0300

    Classes #3 to  #6 - navigation/redux/login

---
+++ src/screens/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Header, HeaderTitle } from './styles';
+const Page = () => {
+    return (
+        <Container>
+            <Header> 
+                <HeaderTitle>
+                        DevsUber
+                </HeaderTitle>
+            </Header>
+        </Container>
+    );
+}
+
+export default Page;
