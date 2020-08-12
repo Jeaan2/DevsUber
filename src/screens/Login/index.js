db0e31122255373f1716afad6906dbba1de29ce4
Jean Silva <jeansilva@MacBook-Pro-de-Jean.local>
2020-08-12 19:59:41 -0300

    Class #7 - login tabs

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { Container, Header, HeaderTitle } from './styles';
+import React, {useState} from 'react';
+import { Container, Header, HeaderTitle, Menu, MenuItem, MenuItemText} from './styles';
 const Page = () => {
+    const [activeMenu, setActiveMenu] = useState('signin')
+
+
     return (
         <Container>
             <Header> 
@@ -8,6 +11,16 @@ const Page = () => {
                         DevsUber
                 </HeaderTitle>
             </Header>
+            <Menu>
+                <MenuItem active={activeMenu == 'signin'} onPress={() => setActiveMenu('signin')} underlayColor="transparent">
+                    <MenuItemText>Login</MenuItemText>
+                </MenuItem>
+                <MenuItem active={activeMenu == 'signup'} onPress={() => setActiveMenu('signup')}>
+                    <MenuItemText>Cadastrar</MenuItemText>
+                </MenuItem>
+            </Menu>
+
+            
         </Container>
     );
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -8,10 +8,25 @@ export const Header = styled.View`
     height: 150px;
     background-color: #3574CB;
     justify-content: center;
-    padding-left: 20px
+    padding-left: 20px;
 `;
 
 export const HeaderTitle = styled.Text`
     color: #fff;
     font-size: 27px;
 `;
+
+export const Menu = styled.View`
+  background-color: #3574CB;
+  flex-direction: row;
+  padding-left: 20px;
+`;
+export const MenuItem = styled.TouchableHighlight`
+  padding: 20px;
+  border-bottom-width: 5px;
+  border-bottom-color: ${props => props.active? '#FFF' : '#3574CB'};
+`;
+export const MenuItemText = styled.Text`
+    color: #FFF;
+    font-size: 16px;
+`;  
